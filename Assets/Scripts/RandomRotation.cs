e6123265d473df1ad664e451418b484164795015
Ripley-426 <nkotolenko@gmail.com>
2021-11-18 11:23:01 -0300

    Added sprites + Rotation script
    3 different obstacle sprites + 2 sound effects + RandomRotation.cs

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomRotation : MonoBehaviour
+{
+    public int rotationDirection;
+    [SerializeField] public float rotationSpeed = 5f;
+    private GameObject _go;
+
+    private void Awake()
+    {
+        _go = gameObject;
+        rotationDirection = SetRotationDirection();
+    }
+
+    private int SetRotationDirection()
+    {
+        if (5 < Random.Range(0, 10))
+        {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    private void Update()
+    {
+        ApplyRotation();
+    }
+
+    private void ApplyRotation()
+    {
+        transform.Rotate(0,0, rotationDirection * rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,12 +7,18 @@ public class ShipController : MonoBehaviour
 {
     [SerializeField] private new Rigidbody2D rigidbody;
     [SerializeField] private float speed = 1f;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip shootingSound;
     
     private Vector2 _position;
     private Translator _trans;
 
+    [SerializeField] private float shootingCooldown = 1f;
+    private float _currentTime;
+
     private void Awake()
     {
+        _currentTime = shootingCooldown;
         _trans = gameObject.AddComponent<Translator>();
         _position = transform.position;
         SetRandomXPosition();
@@ -23,6 +29,20 @@ public class ShipController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         _trans.Move(new Vector2(moveHorizontal, moveVertical) * speed);
+        
+        ShootLogic();
+    }
+
+    private void ShootLogic()
+    {
+        if (_currentTime < shootingCooldown)
+        {
+            _currentTime += Time.deltaTime;
+            return;
+        }
+        if (!Input.GetKey(KeyCode.Space)) return;
+        _currentTime = 0;
+        audioSource.PlayOneShot(shootingSound);
     }
 
     private void SetRandomXPosition()
